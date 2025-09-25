cfddbf3778d3974c64b7b2a2481b87daa0e9da12
Ankit Singh <ankitoct03@gmail.com>
2025-09-25 17:34:48 +0530

    codeforces problemset

---
+++ 50A/50A.cpp
@@ -0,0 +1,15 @@
+// Domino piling
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n, area;
+    cin >> m >> n;
+
+    area = (m * n) / 2;
+
+    cout << area;
+    return 0;
+}
