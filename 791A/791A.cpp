649d93374d9d4acf1bbb6f572d5a015a9d68f7e2
Ankit Singh <ankitoct03@gmail.com>
2025-09-28 19:44:14 +0530

    easy question

---
+++ 791A/791A.cpp
@@ -0,0 +1,21 @@
+// Bear and Big Brother
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int count = 0;
+
+    for (int i = 0; a <= b; ++i)
+    {
+        a = a * 3;
+        b = b * 2;
+        count++;
+    }
+
+    cout << count;
+    return 0;
+}
