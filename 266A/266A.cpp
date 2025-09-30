c4996b39fe6491f1e2e9c3894e6aac5a6ba180a8
Ankit Singh <ankitoct03@gmail.com>
2025-09-30 13:01:16 +0530

    Easy question

---
+++ 266A/266A.cpp
@@ -0,0 +1,23 @@
+// Stones on the Table
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int count = 0;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == s[i + 1])
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
